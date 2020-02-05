11579a694274728b1bc1ab8ceb0fae665f641cc7
phtf92 <phtf92@gmail.com>
2020-02-05 19:07:56 -0300

    add filme

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import Header from './components/Header';
 import MovieLibrary from './components/MovieLibrary';
 import movies from './data';
+import AddMovie from './components/AddMovie'
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MovieLibrary movies={movies} />
+      <AddMovie />
     </div>
   );
 }
--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,58 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+  trocarEstado = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+  addFilme = (event) => {
+    event.preventDefault();
+    console.log(this.props)
+    this.setState({
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    })
+  }
   render() {
+    const propriedades = this.state;
+    console.log(propriedades)
+    return (
+      <form>
+        <label>Título</label><br></br>
+        <input name='title' type='text' value={propriedades.title} onChange={this.trocarEstado} /><br></br>
+        <label>Subtítulo</label><br></br>
+        <input name='subtitle' type='text' value={propriedades.subtitle} onChange={this.trocarEstado} /><br></br>
+        <label>Imagem</label><br></br>
+        <input name='imagePath' type='text' value={propriedades.imagePath} onChange={this.trocarEstado} /><br></br>
+        <label>Sinopse</label><br></br>
+        <textarea name='storyline' value={propriedades.storyline} onChange={this.trocarEstado} /><br></br>
+        <label>Avaliação</label><br></br>
+        <input name='rating' type='number' value={propriedades.rating} onChange={this.trocarEstado} /><br></br>
+
+        <label>Gênero</label><br></br>
+        <select name='genre' onChange={this.trocarEstado} value={propriedades.genre}>
+          <option value='action'>Ação</option>
+          <option value='comedy'>Comédia</option>
+          <option value='thriller'>Suspense</option>
+        </select><br></br>
+        <button onClick={this.addFilme}>Adicionar filme</button>
+      </form>
+    )
   }
 }
 
