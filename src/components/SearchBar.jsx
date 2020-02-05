b2b6bf44e560ca36fd33ae71f6e7a837244a88c1
guicgs <etohlif@protonmail.com>
2020-02-05 15:42:07 -0300

    SearchBar concluída com os testes completos]

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class SearchBar extends Component {
+class SearchBar extends React.Component {
   render() {
+    const { searchText } = this.props;
+    const { onSearchTextChange } = this.props;
+    const { bookmarkedOnly } = this.props;
+    const { onBookmarkedChange } = this.props;
+    const { selectedGenre } = this.props;
+    const { onSelectedGenreChange } = this.props;
+    return (
+      <form>
+        <label htmlFor="includes-text">
+          Inclui o texto:
+          <input
+            type="text"
+            value={searchText}
+            onChange={onSearchTextChange}
+            id="includes-text"
+          />
+        </label>
+        <label htmlFor="show-favs">
+          Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="show-favs"
+            checked={bookmarkedOnly}
+            onChange={onBookmarkedChange}
+          />
+          <label htmlFor="gender-filter">
+            Filtrar por gênero
+            <select value={selectedGenre} onChange={onSelectedGenreChange}>
+              <option value="">Todos</option>
+              <option value="action">Ação</option>
+              <option value="comedy">Comédia</option>
+              <option value="thriller">Suspense</option>
+            </select>
+          </label>
+        </label>
+      </form>
+    );
   }
 }
 
