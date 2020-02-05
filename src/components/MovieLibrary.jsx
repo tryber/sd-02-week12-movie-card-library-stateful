bc9fa44bd3aeb1e6e742d1342c416020bf927ac3
EddyeBoy27 <lipe0503@gmail.com>
2020-02-05 16:09:35 -0300

    Iniciando Projeto

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
+import SearchBar from './SearchBar'
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    }
+  }
+
   render() {
-    return <h2>My awesome Movie Library</h2>;
+    return (
+      <div>
+        <SearchBar 
+          searchText={this.state.searchText}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          selectedGenre={this.state.selectedGenre}
+        />
+        <h2>My awesome Movie Library</h2>
+      </div>
+    );
   }
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    return (
+      <div>
+        
+      </div>
+    )
   }
 }
 
