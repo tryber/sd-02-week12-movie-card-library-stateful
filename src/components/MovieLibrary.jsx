c384f47b2541a65cba1cd636b810dec0426b5e17
lauro <laurolyra@gmail.com>
2020-02-05 16:24:34 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
+import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import AddMovie from './AddMovie';
+
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <h2>My awesome Movie Library</h2>;
+    return (
+      <div>
+        <h2> My awesome movie library </h2>
+        <SearchBar />
+        <MovieList movies={this.props.movies} />
+        <AddMovie />
+      </div>
+    );
   }
 }
 
