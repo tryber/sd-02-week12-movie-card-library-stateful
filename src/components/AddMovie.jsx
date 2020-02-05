dca2e79722342085cbad3b1a391e0b32a520be28
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-05 18:07:23 -0300

    construindo componentes

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,101 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle:"",
+      title:"",
+      imagePath:"",
+      storyline:"",
+      rating:0,
+      genre:'action',
+    };
+  }
+
+  this.handleInputChange = this.handleInputChange.bind(this);
+}
+
+handleInputChange(event) {
+  const target = event.target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+  const name = target.name;
+
+  this.setState({
+    [name]: value,
+  });
+}
+
+
   render() {
+    <form>
+    <label>
+      Título
+      <input
+        name='title'
+        type='text'
+        value={this.state.title}
+        onChange={this.handleInputChange}
+      />
+    </label>
+
+
+
+    <label>
+      Subtítulo
+      <input
+        name='subtitle'
+        type='text'
+        value={this.state.subtitle}
+        onChange={this.handleInputChange}
+      />
+    </label>
+
+
+
+    <label>
+      Imagem
+      <input
+        name='imagePath'
+        type='text'
+        value={this.state.imagePath}
+        onChange={this.handleInputChange}
+      />
+    </label>
+
+
+    <label>
+          Sinopse:
+          <textarea value={this.state.storyline} onChange={this.handleInputChange} name='storyline'} />
+          
+        </label>
+
+
+    <label>
+      Avaliação
+      <input
+        name='rating'
+        type='number'
+        value={this.state.rating}
+        onChange={this.handleInputChange}
+      />
+    </label>
+
+    <label>
+      Gênero
+      <select value={this.state.genre} onChange={this.handleInputChange} name='genre'>
+        <option value='action'>Ação</option>
+        <option value='comedy'>Comédia</option>
+        <option value='thriller'>Suspense</option>
+      </select>
+    </label>
+
+    <button onClick={activateLasers}>
+  Adicionar Filme
+</button>
+
+    
+  </form>
   }
 }
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,58 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    return (
+      <form>
+        <label>
+          Inclui o texto:
+          <input
+            name='isGoing'
+            type='text'
+            value={this.searchText}
+            onChange={this.onSearchTextChange}
+          />
+        </label>
+        <label>
+          Mostrar somente favoritos
+          <input
+            name='isGoing'
+            type='checkbox'
+            checked={this.bookmarkedOnly}
+            onChange={this.onBookmarkedChange}
+          />
+        </label>
+        <label>
+          Filtrar por gênero
+          <select value={this.selectGenre} onChange={this.onSelectedGenreChange}>
+            <option value="">Todos</option>
+            <option value='action'>Ação</option>
+            <option value='comedy'>Comédia</option>
+            <option value='thriller'>Suspense</option>
+          </select>
+        </label>
+      </form>
+    );
   }
 }
 
