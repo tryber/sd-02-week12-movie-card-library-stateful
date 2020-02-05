4f375ba36f1c7e419c847e37c0aaf2461cacbc74
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-05 17:40:17 -0300

    levando couro

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,12 +5,13 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
+      
       <div>
         <h2> My awesome movie library </h2>
         <SearchBar />
