5080ea929c89d805dca7adca133afccf08535fba
Douglas Henrique <doughrqe@gmail.com>
2020-02-05 16:00:30 -0300

    Iniciando SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -5,17 +5,45 @@ import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
 
 class MovieLibrary extends Component {
+  constructor(props) {
+    super(props);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+    }
+  }
+
+  onSearchTextChange(e) {
+    console.log(e.target.value);
+  }
+
+  onBookmarkedChange(e) {
+    console.log(e.target.checked);
+  }
+
+  onSelectedGenreChange(e) {
+    console.log(e.target.value);
+  }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
-        <MovieList movies={this.props.movies} />
+        <SearchBar
+          selectedGenre={this.onSelectedGenreChange}
+          BookmarkedChange={this.onBookmarkedChange}
+          SearchTextChange={this.onSearchTextChange}
+        />
+        <MovieList
+          movies={this.props.movies}
+        />
         <AddMovie />
       </div>
     );
   }
 }
-
 export default MovieLibrary;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,7 +2,32 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
-    return <p>a</p>;
+    const { selectedGenre, BookmarkedChange, SearchTextChange } = this.props;
+    return (
+      <div>
+        <form>
+          <label>
+            <p> Inclui o Texto</p>
+            <input type="text" name="InputText" onChange={(e) => SearchTextChange(e)} />
+          </label>
+          <br />
+          <label>
+            <input type="checkbox" onChange={(e) => BookmarkedChange(e)} />
+            Mostrar Somente Favoritos
+          </label>
+          <br />
+          <label>
+            Filtrar Por Genero
+            <select name="select" onChange={(e) => selectedGenre(e)}>
+              <option value="">Todos</option>
+              <option value="1">Ação</option>
+              <option value="2">Comedia</option>
+              <option value="3">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
+    );
   }
 }
 
