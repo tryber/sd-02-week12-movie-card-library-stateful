cdc312b9a2cbbc785f29ba818de2c71644aa3fcd
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-02-05 20:03:12 -0300

    AddMovie faltando apenas o botão

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,9 +1,84 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
+      rating: 0,
+      genre: 'action',
+    }
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+
+  changeInput (e, nome) {
+    const { value } = e.target;
+    this.setState(() => ({ [nome]: value, }));
+    if (nome === 'rating') {
+      this.setState(() => ({ [nome]: parseFloat(value), }));
+    }
+  }
+
+  inputText (label, type, item, func) {
     return (
-      <p>AddMovie Stub</p>
+      <label>
+        {label}
+        <input
+          type={type}
+          name={item}
+          value={this.state[item]}
+          onChange={(e) => func(e, item)}
+        />
+      </label>
+    );
+  }
+
+  inputTextArea (label, item, func) {
+    return (
+      <label>
+        {label}
+        <textarea
+          name={item}
+          value={this.state[item]}
+          onChange={(e) => func(e, item)}
+        />
+      </label>
+    );
+  }
+
+  selectBox (item) {
+    return (
+      <label>
+        Gênero:
+        <select
+          onChange={(e) => this.changeInput(e, item)}
+          value={this.state.genre}
+        >
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render () {
+    return (
+      <form>
+        <fieldset>
+          {this.inputText('Título', 'text', 'title', this.changeInput)}
+          {this.inputText('Subtítulo', 'text', 'subtitle', this.changeInput)}
+          {this.inputText('Imagem', 'text', 'imagePath', this.changeInput)}
+          {this.inputTextArea('Sinopse', 'storyline', this.changeInput)}
+          {this.inputText('Avaliação', 'number', 'rating', this.changeInput)}
+          {this.selectBox('genre')}
+          <button>Adicionar filme</button>
+        </fieldset>
+      </form>
     );
   }
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,40 +1,59 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-  render() {
+  inputText () {
+    return (
+      <label>
+        Inclui o texto:
+        <input
+          type="text"
+          onChange={this.props.onSearchTextChange}
+          value={this.props.searchText}
+        />
+      </label>
+    );
+  }
+
+  inputCheckbox () {
+    return (
+      <label>
+        <input
+          type="checkbox"
+          onChange={this.props.onBookmarkedChange}
+          checked={this.props.bookmarkedOnly}
+        />
+        Mostrar somente favoritos:
+    </label>
+    );
+  }
+
+  selectBox () {
+    return (
+      <label>
+        Filtrar por gênero:
+        <select
+          onChange={this.props.onSelectedGenreChange}
+          value={this.props.selectedGenre}
+        >
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
+  }
+
+  render () {
     return (
       <form>
-        <label>
-          Inclui o texto:
-          <input
-            type="text"
-            onChange={this.props.onSearchTextChange}
-            value={this.props.searchText}
-          />
-        </label>
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            onChange={this.props.onBookmarkedChange}
-            checked={this.props.bookmarkedOnly}
-          />
-          Mostrar somente favoritos:
-        </label>
-        <br />
-        <label>
-        Filtrar por gênero:
-          <select
-            onChange={this.props.onSelectedGenreChange}
-            value={this.props.selectedGenre}
-          >
-            <option value="">Todos</option>
-            <option value="action">Ação</option>
-            <option value="comedy">Comédia</option>
-            <option value="thriller">Suspense</option>
-          </select>
-        </label>
-
+        <fieldset>
+          {this.inputText()} 
+          <br />
+          {this.inputCheckbox()}
+          <br />
+          {this.selectBox()}
+        </fieldset>
       </form>
     );
   }
