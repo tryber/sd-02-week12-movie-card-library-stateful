f814378eb13dd9e5622124f412e75e5518fa887f
Julio Cezar Taveira Araujo <jctaraujo@hotmail.com>
2020-02-05 15:53:41 -0200

    Adicionado state em MovieLibrarys, Adicionado callbacks em SearchBar

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -7,15 +7,43 @@ import AddMovie from './AddMovie';
 class MovieLibrary extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+      bookmarkedOnly: false,
+      selectedGenre: '',
+      movies: this.props.movies,
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
+  }
+
+  onSearchTextChange(event) {
+    console.log(event.target.value);
+  }
+
+  onBookmarkedChange(event) {
+    console.log(event.target.checked);
+  }
+
+  onSelectedGenreChange(event) {
+    console.log(event.target.value);
   }
 
   render() {
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          text={this.state.searchText}
+          changeText={this.onSearchTextChange}
+          bookmarkedOnly={this.state.bookmarkedOnly}
+          bookmarkedChange={this.onBookmarkedChange}
+          genre={this.state.selectedGenre}
+          genreSelected={this.onSelectedGenreChange}
+        />
         <MovieList movies={this.props.movies} />
-        <AddMovie />
+        <AddMovie addMovie={this.addNewMovie} />
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,8 +2,31 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
   render() {
+    const { changeText, bookmarkedChange, genreSelected } = this.props;
     return (
-      <p>SearchBar</p>
+      <div>
+        <form>
+          <label>
+            SearchBar:
+            <input type="text" name="searchBarText" onChange={(e) => changeText(e)} />
+          </label>
+          <br />
+          <label>
+            Favoritos:
+            <input type="checkbox" name="bookMarked" onChange={(e) => bookmarkedChange(e)} />
+          </label>
+          <br />
+          <label>
+            Genero:
+            <select name="select" onChange={(e) => genreSelected(e)}>
+              <option value="">Todos</option>
+              <option value="action">Acao</option>
+              <option value="comedy">Comedia</option>
+              <option value="thriller">Suspense</option>
+            </select>
+          </label>
+        </form>
+      </div>
     );
   }
 }
