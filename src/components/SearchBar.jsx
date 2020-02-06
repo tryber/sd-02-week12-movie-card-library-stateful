295de88f3e0d997149a56d5c66dd07a46d355c1d
phtf92 <phtf92@gmail.com>
2020-02-06 11:17:28 -0300

    Code climate

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,15 +6,20 @@ class SearchBar extends Component {
     const propriedades = this.props;
     return (
       <form>
-        <label>Inclui o texto</label><br />
+        <label htmlFor='name'>Inclui o texto</label><br />
         <input name="searchText" type="text" value={propriedades.searchText}
-          onChange={propriedades.onSearchTextChange} /><br />
-        <input name="bookmarkedOnly" type="checkbox" checked={propriedades.bookmarkedOnly}
-          onChange={propriedades.onBookmarkedChange} />
-        <label>Mostrar somente favoritos</label><br />
-        <label>Filtrar por gênero</label><br />
-        <select name="selectedGenre" onChange={propriedades.onSelectedGenreChange}
-          value={propriedades.selectedGenre}>
+          onChange={propriedades.onSearchTextChange} id='name'
+        /><br />
+        <input
+          name="bookmarkedOnly" type="checkbox" checked={propriedades.bookmarkedOnly}
+          onChange={propriedades.onBookmarkedChange} id='bookmark'
+        />
+        <label htmlFor='bookmark'>Mostrar somente favoritos</label><br />
+        <label htmlFor='genero'>Filtrar por gênero</label><br />
+        <select
+          id='genero' name="selectedGenre" onChange={propriedades.onSelectedGenreChange}
+          value={propriedades.selectedGenre}
+        >
           <option value="">Todos</option>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
