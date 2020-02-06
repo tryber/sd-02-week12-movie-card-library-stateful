792aa0b65c116e961899bc60af8d1c16a9216435
Douglas Henrique <doughrqe@gmail.com>
2020-02-05 22:40:46 -0300

    iniciando ideia do movie library

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -19,22 +19,33 @@ class MovieLibrary extends Component {
   }
 
   onSearchTextChange(e) {
+    const arrMovies = this.props.movies;
+    const { searchText } = this.state;
     const { value } = e.target;
-    this.setState(() => ({ searchText: value }))
+    this.setState(() => ({
+      searchText: value,
+      movies: arrMovies.filter(element => {
+        return (element.title.includes(searchText) || element.subtitle.includes(searchText) ||
+          element.storyline.includes(searchText));
+      })
+    }));
   }
 
   onBookmarkedChange(e) {
     const { checked } = e.target;
-    this.setState(() => ({ bookmarkedOnly: checked }));
+    this.setState(() => ({ bookmarkedOnly: checked }))
+    this.setState((state) => ({ movies: (state.bookmarkedOnly) ? state.movies.filter(element => element.bookmarked === state.bookmarkedOnly) : this.props.movies }));
   }
 
   onSelectedGenreChange(e) {
     const { value } = e.target;
-    this.setState(() => ({ selectedGenre: value }))
+    this.setState(() => ({ selectedGenre: value }));
+    this.setState((state) => ({ movies: state.movies.filter(element => element.genre === state.selectedGenre) 
+    }));
   }
 
   render() {
-    const {searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre, movies } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
