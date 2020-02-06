cac4cd21963836e531be4851fe9ef30ae64aeeda
phtf92 <phtf92@gmail.com>
2020-02-06 12:04:54 -0300

    Code climate

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react';
 
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      subtitle: "",
-      title: "",
-      imagePath: "",
-      storyline: "",
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyline: '',
       rating: 0,
-      genre: "action",
+      genre: 'action',
     };
-    this.trocarTitle = this.trocarTitle.bind(this)
-    this.trocarSubtitle = this.trocarSubtitle.bind(this)
-    this.trocarImagePath = this.trocarImagePath.bind(this)
-    this.trocarStoryline = this.trocarStoryline.bind(this)
-    this.trocarRating = this.trocarRating.bind(this)
-    this.trocarGenre = this.trocarGenre.bind(this)
+    this.trocarTitle = this.trocarTitle.bind(this);
+    this.trocarSubtitle = this.trocarSubtitle.bind(this);
+    this.trocarImagePath = this.trocarImagePath.bind(this);
+    this.trocarStoryline = this.trocarStoryline.bind(this);
+    this.trocarRating = this.trocarRating.bind(this);
+    this.trocarGenre = this.trocarGenre.bind(this);
   }
   trocarTitle(event) {
-    this.setState({ title: event.target.value })
+    this.setState({ title: event.target.value });
   }
   trocarSubtitle(event) {
-    this.setState({ subtitle: event.target.value })
+    this.setState({ subtitle: event.target.value });
   }
   trocarImagePath(event) {
-    this.setState({ imagePath: event.target.value })
+    this.setState({ imagePath: event.target.value });
   }
   trocarStoryline(event) {
-    this.setState({ storyline: event.target.value })
+    this.setState({ storyline: event.target.value });
   }
   trocarRating(event) {
-    this.setState({ rating: parseFloat(event.target.value) })
+    this.setState({ rating: parseFloat(event.target.value) });
   }
   trocarGenre(event) {
-    this.setState({ genre: event.target.value })
+    this.setState({ genre: event.target.value });
   }
 
 
@@ -51,28 +51,38 @@ class AddMovie extends React.Component {
   // }
   render() {
     const estado = this.state;
-    console.log(estado)
     return (
       <form>
-        <label>Título</label><br></br>
-        <input name="title" type="text" value={estado.title} onChange={this.trocarTitle} /><br></br>
-        <label>Subtítulo</label><br></br>
-        <input name="subtitle" type="text" value={estado.subtitle} onChange={this.trocarSubtitle} /><br></br>
-        <label>Imagem</label><br></br>
-        <input name="imagePath" type="text" value={estado.imagePath} onChange={this.trocarImagePath} /><br></br>
-        <label>Sinopse</label><br></br>
-        <textarea name="storyline" value={estado.storyline} onChange={this.trocarStoryline} /><br></br>
-        <label>Avaliação</label><br></br>
-        <input name="rating" type="number" value={estado.rating} onChange={this.trocarRating} /><br></br>
-        <label>Gênero</label><br></br>
-        <select name="genre" onChange={this.trocarGenre} value={estado.genre}>
+        <label htmlFor='name'>Título</label><br></br>
+        <input id='name' name="title" type="text" value={estado.title}
+          onChange={this.trocarTitle}
+        /><br />
+        <label htmlFor='sub'>Subtítulo</label><br></br>
+        <input id='sub' name="subtitle" type="text" value={estado.subtitle}
+          onChange={this.trocarSubtitle}
+        /><br />
+        <label htmlFor='img'>Imagem</label><br></br>
+        <input id='img' name="imagePath" type="text" value={estado.imagePath}
+          onChange={this.trocarImagePath}
+        /><br />
+        <label htmlFor='sin'>Sinopse</label><br></br>
+        <textarea id='sin' name="storyline" value={estado.storyline}
+          onChange={this.trocarStoryline}
+        /><br />
+        <label htmlFor='av'>Avaliação</label><br></br>
+        <input id='av' name="rating" type="number" value={estado.rating}
+          onChange={this.trocarRating}
+        /><br />
+        <label htmlFor='gen'>Gênero</label><br></br>
+        <select id='gen' name="genre"
+          onChange={this.trocarGenre} value={estado.genre}>
           <option value="action">Ação</option>
           <option value="comedy">Comédia</option>
           <option value="thriller">Suspense</option>
         </select><br />
         <button onClick={this.props.callbackAddFilme}>Adicionar filme</button>
       </form>
-    )
+    );
   }
 }
 
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar'
-import MovieList from './MovieList'
-import AddMovie from './AddMovie'
+import SearchBar from './SearchBar';
+import MovieList from './MovieList';
+import AddMovie from './AddMovie';
 
 
 class MovieLibrary extends Component {
@@ -12,49 +12,52 @@ class MovieLibrary extends Component {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
-      movies: this.props.movies
+      movies: this.props.movies,
     };
-    this.changeHandlerText = this.changeHandlerText.bind(this)
-    this.changeBookMarker = this.changeBookMarker.bind(this)
-    this.changeGenre = this.changeGenre.bind(this)
-    this.filtrarPorText = this.filtrarPorText.bind(this)
-    this.filtrarPorGenero = this.filtrarPorGenero.bind(this)
-    this.filtrarPorBookmark = this.filtrarPorBookmark.bind(this)
-    this.addicionarFilme = this.addicionarFilme.bind(this)
+    this.changeHandlerText = this.changeHandlerText.bind(this);
+    this.changeBookMarker = this.changeBookMarker.bind(this);
+    this.changeGenre = this.changeGenre.bind(this);
+    this.filtrarPorText = this.filtrarPorText.bind(this);
+    this.filtrarPorGenero = this.filtrarPorGenero.bind(this);
+    this.filtrarPorBookmark = this.filtrarPorBookmark.bind(this);
+    this.addicionarFilme = this.addicionarFilme.bind(this);
   }
   changeHandlerText(event) {
-    const { value } = event.target
-    this.setState({ searchText: value })
+    const { value } = event.target;
+    this.setState({ searchText: value });
   }
   changeBookMarker() {
-    const booleano = this.state.bookmarkedOnly
-    const retorno = booleano ? false : true
-    this.setState({ bookmarkedOnly: retorno })
+    const booleano = this.state.bookmarkedOnly;
+    const retorno = booleano ? false : true;
+    this.setState({ bookmarkedOnly: retorno });
   }
   changeGenre(event) {
-    this.setState({ selectedGenre: event.target.value })
+    this.setState({ selectedGenre: event.target.value });
   }
   filtrarPorText(filmes, texto) {
-    return filmes.filter(filme => (filme.title.includes(texto) || filme.subtitle.includes(texto) || filme.storyline.includes(texto)))
+    return filmes.filter(filme => (filme.title.includes(texto) ||
+      filme.subtitle.includes(texto) || filme.storyline.includes(texto)));
   }
   filtrarPorGenero(filmes, genero) {
-    if (!genero) return filmes
-    return filmes.filter(filme => filme.genre === genero)
+    if (!genero) return filmes;
+    return filmes.filter((filme) => filme.genre === genero)
   }
   filtrarPorBookmark(filmes, bookmark) {
-    if (bookmark) return filmes.filter(filme => filme.bookmarked)
-    return filmes
+    if (bookmark) return filmes.filter((filme) => filme.bookmarked);
+    return filmes;
   }
   addicionarFilme(event) {
     event.preventDefault();
-    alert('abc');
+    console.log('abc');
   }
 
   render() {
-    const filmes = this.state.movies
-    const conteudoFiltradoPorTexto = this.filtrarPorText(filmes, this.state.searchText)
-    const conteudoFiltradoPorGenero = this.filtrarPorGenero(conteudoFiltradoPorTexto, this.state.selectedGenre)
-    const conteudoFiltradoPorBookmark = this.filtrarPorBookmark(conteudoFiltradoPorGenero, this.state.bookmarkedOnly)
+    const filmes = this.state.movies;
+    const conteudoFiltradoPorTexto = this.filtrarPorText(filmes, this.state.searchText);
+    const conteudoFiltradoPorGenero = this.
+      filtrarPorGenero(conteudoFiltradoPorTexto, this.state.selectedGenre);
+    const conteudoFiltradoPorBookmark = this.
+      filtrarPorBookmark(conteudoFiltradoPorGenero, this.state.bookmarkedOnly);
     return (
       <div>
         <h2>My awesome Movie Library</h2>
@@ -68,7 +71,7 @@ class MovieLibrary extends Component {
         />
         <MovieList movies={conteudoFiltradoPorBookmark} />
         <AddMovie callbackAddFilme={this.addicionarFilme} />
-      </div>)
+      </div>);
   }
 }
 
