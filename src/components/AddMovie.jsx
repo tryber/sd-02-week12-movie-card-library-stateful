85b326666e1471f780afc3fc55b5970412d5bbd2
Andrey <andreytsuzuki@gmail.com>
2020-02-06 19:48:50 -0300

    fix

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -48,28 +48,26 @@ class AddMovie extends React.Component {
 
   render() {
     return (
-      <>
-        <form onSubmit={this.submithandler}>
-            <label>Título</label>
-            <input type='text' name='title' value={this.state.title} onChange={(e) => this.onChange(e, 'title')} />
-            <label>Subtítulo</label>
-            <input type='text' name='subtitle' value={this.state.subtitle} onChange={(e) => this.onChange(e, 'subtitle')} />
-            <label>Imagem</label>
-            <input type='text' value={this.state.imagePath} onChange={(e) => this.onChange(e, 'imagePath')} />
-            <label>Sinopse</label>
-            <textarea value={this.state.storyline} onChange={(e) => this.onChange(e, 'storyline')} ></textarea>
-            <label>Avaliação</label>
-            <input type='number' value={this.state.rating} onChange={(e) => this.onChange(e, 'rating')} />
-            <label>Gênero</label>
-            <select value={this.state.genre} onChange={(e) => this.onChange(e, 'genre')}>
-              {genreOptions.map(option => (
-                <option key={option.value} value={option.value}>
-                  {option.text}
-                </option>))}
-            </select>
-          <button type='submit' onClick={this.onClick}>Adicionar filme</button>
-        </form>
-      </>
+      <form onSubmit={this.submithandler}>
+        <label htmlFor='title'>Título</label>
+        <input id='title' type='text' name='title' value={this.state.title} onChange={(e) => this.onChange(e, 'title')} />
+        <label htmlFor='subtitle'>Subtítulo</label>
+        <input id='subtitle' type='text' name='subtitle' value={this.state.subtitle} onChange={(e) => this.onChange(e, 'subtitle')} />
+        <label htmlFor='image'>Imagem</label>
+        <input id='image' type='text' value={this.state.imagePath} onChange={(e) => this.onChange(e, 'imagePath')} />
+        <label htmlFor='sinopse'>Sinopse</label>
+        <textarea id='sinopse' value={this.state.storyline} onChange={(e) => this.onChange(e, 'storyline')} ></textarea>
+        <label htmlFor='rating'>Avaliação</label>
+        <input id='rating' type='number' value={this.state.rating} onChange={(e) => this.onChange(e, 'rating')} />
+        <label htmlFor='genre'>Gênero</label>
+        <select id='genre' value={this.state.genre} onChange={(e) => this.onChange(e, 'genre')}>
+          {genreOptions.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.text}
+            </option>))}
+        </select>
+        <button type='submit' onClick={this.onClick}>Adicionar filme</button>
+      </form>
     )
   }
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,31 +9,25 @@ const genreOptions = [
 
 class SearchBar extends Component {
   render() {
-    const { searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange } = this.props;
+    const {
+      searchText, onSearchTextChange, bookmarkedOnly,
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange
+    } = this.props;
     return (
-      <div>
-        <form>
-          <div>
-            <label>Inclui o texto</label>
-            <input type="text" value={searchText} onChange={onSearchTextChange} />
-          </div>
-          <div>
-            <label>Mostrar somente favoritos</label>
-            <input type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
-          </div>
-          <div>
-            <label>Filtrar por gênero</label>
-            <select value={selectedGenre} onChange={onSelectedGenreChange}>
-              {genreOptions.map(option => (
-                <option key={option.value} value={option.value} >
-                  {option.text}
-                </option>
-              ))}
-            </select>
-          </div>
-        </form>
-      </div>
+      <form>
+        <label htmlFor='search'>Inclui o texto</label>
+        <input id='search' type="text" value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor='bookmarked'>Mostrar somente favoritos</label>
+        <input id='bookmarked' type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+        <label htmlFor='genre'>Filtrar por gênero</label>
+        <select id='genre' value={selectedGenre} onChange={onSelectedGenreChange}>
+          {genreOptions.map(option => (
+            <option key={option.value} value={option.value} >
+              {option.text}
+            </option>
+          ))}
+        </select>
+      </form>
     )}
 }
 
