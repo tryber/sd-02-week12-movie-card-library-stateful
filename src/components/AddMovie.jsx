4a95c49fa4ada4a62c8f042d53228b37e1572316
Lucas-Werner <lucas.1werner@gmail.com>
2020-02-06 14:23:33 -0300

    refatorando

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,83 +11,118 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.titleChange = this.titleChange.bind(this);
+    this.subtitleChange = this.subtitleChange.bind(this);
+    this.imageChange = this.imageChange.bind(this);
+    this.storylineChange = this.storylineChange.bind(this);
+    this.ratingChange = this.ratingChange.bind(this);
+    this.genreChange = this.genreChange.bind(this);
   }
 
-  handleInputChange() {
-    event => {
-      const target = event.target;
-      const value = target.value;
-      const name = target.name;
+  createInputTitle() {
+    const { title } = this.state;
+    return (
+      <label htmlFor="title">
+        Título
+        <input
+          id="title"
+          name="title"
+          type="text"
+          value={title}
+          onChange={this.titleChange}
+        />
+      </label>
+    );
+  }
 
-      this.setState(() => ({
-        [name]: value,
-      }));
-    };
+  createInputSubtitle() {
+    const { subtitle } = this.state;
+    return (
+      <label htmlFor="subtitle">
+        Subtítulo
+        <input
+          id="subtitle"
+          name="subtitle"
+          type="text"
+          value={subtitle}
+          onChange={this.subtitleChange}
+        />
+      </label>
+    );
+  }
+
+  createImage() {
+    const { imagePath } = this.state;
+    return (
+      <label htmlFor="imagePath">
+        Imagem
+        <input
+          id="imagePath"
+          name="imagePath"
+          type="text"
+          value={imagePath}
+          onChange={this.imageChange}
+        />
+      </label>
+    );
+  }
+
+  createSynopsis() {
+    const { storyline } = this.state;
+    return (
+      <label htmlFor="storyline">
+        Sinopse:
+        <textarea
+          id="storyline"
+          value={storyline}
+          onChange={this.storylineChange}
+          name="storyline"
+        />
+      </label>
+    );
+  }
+
+  createRating() {
+    const { rating } = this.state;
+    return (
+      <label htmlFor="rating">
+        Avaliação
+        <input
+          name="rating"
+          type="number"
+          value={rating}
+          onChange={this.ratingChange}
+        />
+      </label>
+    );
+  }
+
+  createGender() {
+    const { genre } = this.state;
+    return (
+      <label htmlFor="genre">
+        Gênero
+        <select value={genre} onChange={this.genreChange} name="genre" id="genre">
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+    );
   }
 
   render() {
     const { onClick } = this.props;
     return (
       <form>
-        <label>
-          Título
-          <input
-            name='title'
-            type='text'
-            value={this.state.title}
-            onChange={this.handleInputChange()}
-          />
-        </label>
-        <label>
-          Subtítulo
-          <input
-            name='subtitle'
-            type='text'
-            value={this.state.subtitle}
-            nst
-            onChange={this.handleInputChange()}
-          />
-        </label>
-        <label>
-          Imagem
-          <input
-            name='imagePath'
-            type='text'
-            value={this.state.imagePath}
-            onChange={this.handleInputChange()}
-          />
-        </label>
-        <label>
-          Sinopse:
-          <textarea
-            value={this.state.storyline}
-            onChange={this.handleInputChange()}
-            name='storyline'
-          />
-        </label>
-        <label>
-          Avaliação
-          <input
-            name='rating'
-            type='number'
-            value={this.state.rating}
-            onChange={this.handleInputChange()}
-          />
-        </label>
-
-        <label>
-          Gênero
-          <select
-            value={this.state.genre}
-            onChange={this.handleInputChange}
-            name='genre'
-          >
-            <option value='action'>Ação</option>
-            <option value='comedy'>Comédia</option>
-            <option value='thriller'>Suspense</option>
-          </select>
-        </label>
-        <button onClick={onClick}>Adicionar Filme</button>
+        {this.createInputTitle}
+        {this.createInputSubtitle}
+        {this.createImage}
+        {this.createSynopsis}
+        {this.createRating}
+        {this.createGender}
+        <button onClick={onClick} type="button"> Adicionar filme </button>
       </form>
     );
   }
