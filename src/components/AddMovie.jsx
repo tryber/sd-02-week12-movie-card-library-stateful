9b18dfdedb3b3a1d9b304cfec2b5a96593c18bb3
Lauro Lyra Aguiar <50916624+laurolyra@users.noreply.github.com>
2020-02-06 17:24:50 -0300

    Implementações no AddMovie

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(event) {
+    this.setState({
+      title: event.target.value,
+    })
+  }
   render() {
+    return (
+      <form>
+        <label>Título:</label>
+        <input type="text" value={this.state.title} onChange={this.handleChange} />
+      </form>
+    )
   }
 }
 
