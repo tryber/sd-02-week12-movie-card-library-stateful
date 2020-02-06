c90621ccf8fd9b671b4c1571c3967f29d4afe17a
Andrey <andreytsuzuki@gmail.com>
2020-02-06 19:56:12 -0300

    fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,16 +11,16 @@ class SearchBar extends Component {
   render() {
     const {
       searchText, onSearchTextChange, bookmarkedOnly,
-      onBookmarkedChange, selectedGenre, onSelectedGenreChange
+      onBookmarkedChange, selectedGenre, onSelectedGenreChange,
     } = this.props;
     return (
       <form>
-        <label htmlFor='search'>Inclui o texto</label>
-        <input id='search' type="text" value={searchText} onChange={onSearchTextChange} />
-        <label htmlFor='bookmarked'>Mostrar somente favoritos</label>
-        <input id='bookmarked' type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
-        <label htmlFor='genre'>Filtrar por gênero</label>
-        <select id='genre' value={selectedGenre} onChange={onSelectedGenreChange}>
+        <label htmlFor="search">Inclui o texto</label>
+        <input id="search" type="text" value={searchText} onChange={onSearchTextChange} />
+        <label htmlFor="bookmarked">Mostrar somente favoritos</label>
+        <input id="bookmarked" type="checkbox" checked={bookmarkedOnly} onChange={onBookmarkedChange}/>
+        <label htmlFor="genre">Filtrar por gênero</label>
+        <select id="genre" value={selectedGenre} onChange={onSelectedGenreChange}>
           {genreOptions.map(option => (
             <option key={option.value} value={option.value} >
               {option.text}
@@ -28,7 +28,8 @@ class SearchBar extends Component {
           ))}
         </select>
       </form>
-    )}
+    )
+  }
 }
 
 export default SearchBar;
