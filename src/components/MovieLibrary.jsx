684de5651b3c067f74640afb45fb80bbafee587c
EddyeBoy27 <lipe0503@gmail.com>
2020-02-09 20:24:53 -0300

    Issues Fix CC MovieLibrary

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import SearchBar from './SearchBar'
+import SearchBar from './SearchBar';
 import MovieList from './MovieList';
 import AddMovie from './AddMovie';
 
@@ -11,7 +11,7 @@ class MovieLibrary extends Component {
       bookmarkedOnly: false,
       selectedGenre: '',
       movies: this.props.movies,
-    }
+    };
     this.updateSearchText = this.updateSearchText.bind(this);
     this.updateFavorite = this.updateFavorite.bind(this);
     this.updateGenre = this.updateGenre.bind(this);
@@ -20,36 +20,30 @@ class MovieLibrary extends Component {
   }
 
   procuraTexto() {
-    this.setState(() => {
-      return this.state.searchText
-    });
+    this.setState( this.state.searchText );
   }
 
   updateSearchText(event) {
     const { value } = event.target;
-    this.setState({
-      searchText: value,
-    });
+    this.setState({ searchText: value });
   }
 
   updateFavorite(event) {
     const { checked } = event.target;
-    this.setState({
-      bookmarkedOnly: checked
-    });
+    this.setState({ bookmarkedOnly: checked });
   }
 
   updateGenre(e) {
     const { value } = e.target;
-    this.setState(() => ({ selectedGenre: value }));
+    this.setState({ selectedGenre: value });
   }
 
   filtraFilmes(valor) {
     const { searchText, bookmarkedOnly, selectedGenre } = this.state;
     let filmes = valor;
-    if (bookmarkedOnly) filmes = filmes.filter(filme => filme.bookmarked === true);
+    if (bookmarkedOnly) filmes = filmes.filter((filme) => filme.bookmarked === true);
     if (searchText.length > 0) {
-    filmes = filmes.filter(filme => {
+    filmes = filmes.filter((filme) => {
       filmes = filme.title.toLowerCase().includes(searchText.toLowerCase()) ||
       filme.subtitle.toLowerCase().includes(searchText.toLowerCase()) ||
       filme.storyline.toLowerCase().includes(searchText.toLowerCase());
@@ -57,7 +51,7 @@ class MovieLibrary extends Component {
     });
     }
     if (selectedGenre.length > 0) {
-      filmes = filmes.filter(filme => filme.genre === selectedGenre);
+      filmes = filmes.filter((filme) => filme.genre === selectedGenre);
       return filmes;
     }
     return filmes;
