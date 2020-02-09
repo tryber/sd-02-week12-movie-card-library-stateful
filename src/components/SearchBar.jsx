46f7ac97dbc376aaec1d64df71ae50d8ec201a01
EddyeBoy27 <lipe0503@gmail.com>
2020-02-09 18:50:22 -0300

    Finalizado CodeClimate SearchBar.jsx

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,40 +1,60 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  includesText() {
+    return (
+      <label htmlFor="search">
+        Inclui o texto:
+        <input
+          type="text"
+          id="search"
+          value={this.props.searchText}
+          onChange={this.props.onSearchTextChange}
+        />
+      </label>
+    );
+  }
+
+  includesFavorites() {
+    return (
+      <label htmlFor="favorito">
+        Mostrar somente favoritos
+          <input
+            type="checkbox"
+            id="favorito"
+            checked={this.props.bookmarkedOnly}
+            onChange={this.props.onBookmarkedChange}
+          />
+      </label>
+    );
+  }
+
+  includesGenero() {
+    return (
+      <label htmlFor="genero">
+        Filtrar por gênero
+        <select value={this.props.selectedGenre} id="genero" onChange={this.props.onSelectedGenreChange}>
+          <option value="">Todos</option>
+          <option value="action">Ação</option>
+          <option value="comedy">Comédia</option>
+          <option value="thriller">Suspense</option>
+        </select>
+      </label>
+     );
+  }
 
   render() {
     return (
       <div>
         <form>
-
-          <label>
-            Inclui o texto:
-          <input type="text" value={this.props.searchText} onChange={this.props.onSearchTextChange}>
-          </input>
-          </label>
-
-          <br></br>
-
-          <label>
-            Mostrar somente favoritos
-            <input type="checkbox" checked={this.props.bookmarkedOnly} onChange={this.props.onBookmarkedChange}>
-            </input>
-          </label>
-
-          <br></br>
-
-          <label>
-            Filtrar por gênero
-            <select value={this.props.selectedGenre} onChange={this.props.onSelectedGenreChange}>
-                <option value="">Todos</option>
-                <option value="action">Ação</option>
-                <option value="comedy">Comédia</option>
-                <option value="thriller">Suspense</option>
-            </select>
-          </label>
+          {this.includesText()}
+          <br />
+          {this.includesFavorites()}
+          <br />
+          {this.includesGenero()}
         </form>
       </div>
-    )
+    );
   }
 }
 
